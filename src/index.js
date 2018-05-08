35d40f4a37b6a0eb376690ae09856215a484012f
the_boy_is_lazy <408653346@qq.com>
2018-05-08 13:56:49 +0800

    webpack4.0+react

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom";
 import './index.css';
 import './index.scss';
-ReactDOM.render(<h1>hello world</h1>,document.getElementById('app'))
+ReactDOM.render(<h1>hello react</h1>,document.getElementById('app'))
 if(module.hot){
 	module.hot.accept()
 }
